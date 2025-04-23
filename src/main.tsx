60ab4489066b752627631ddf507cb8dabecfc268
Mark Wiggans <mmw125@gmail.com>
2025-04-23 18:51:46 -0400

    fix import

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from "@vercel/analytics/react"
 import './index.css'
-import { Spades } from './Spades.js'
+import { Spades } from './spades'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Spades />
+      <Spades />
     <Analytics />
   </StrictMode>,
 )
